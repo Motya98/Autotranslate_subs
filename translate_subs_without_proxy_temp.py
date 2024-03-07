c7fa7ce6781cf292368a8d8af2d436f790c5e510
Motya98 <113919191+Motya98@users.noreply.github.com>
2024-03-07 23:12:45 +0500

    Update translate_subs_without_proxy_temp.py

--- translate_subs_without_proxy_temp.py
+++ translate_subs_without_proxy_temp.py
@@ -6,7 +6,7 @@ from tkinter import *
 import threading
 
 
-def target():
+def target(tempory_text):
     garbage = []
     translator = translate.Translator(from_lang='en', to_lang='ru')
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -14,22 +14,24 @@ def target():
     while True:
         image = pyscreenshot.grab(bbox=(0, 50, 1000, 170))
         text = pytesseract.image_to_string(image, lang='eng')
-        if len(garbage) != 0:
-            garbage[0].destroy()
-            garbage.pop()
-        try:
-            google = translator.translate(text)
-            label = Label(text=google, font=("Arial", 10))
-            label.pack(padx=20, pady=30)
-            garbage.append(label)
-        except:
-            pass
+        if text != tempory_text:
+            try:
+                tempory_text = text
+                google = translator.translate(text)
+                if len(garbage) != 0:
+                    garbage[0].destroy()
+                    garbage.pop()
+                label = Label(text=google, font=("Arial", 10))
+                label.pack(padx=20, pady=30)
+                garbage.append(label)
+            except:
+                pass
         time.sleep(1)
 
-
+tempory_text = []
 root = Tk()
 root.geometry("700x100+0+130")
 root.attributes('-alpha', 0.4)
 root.attributes("-topmost",True)
-threading.Thread(target=target).start()
+threading.Thread(target=target, args=(tempory_text,)).start()
 root.mainloop()
